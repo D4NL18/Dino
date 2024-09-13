feb4d674628f48f0228cd403e8cb9103c71c46d4
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2024-09-13 13:33:32 -0300

    Criação da rede neural e Mutate

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,24 +1,60 @@
 from chrome_trex import DinoGame, ACTION_UP, ACTION_FORWARD, ACTION_DOWN
+from chrome_trex import MultiDinoGame, ACTION_UP, ACTION_FORWARD, ACTION_DOWN
+import numpy as np
 
-# Create a new game that runs with at most 'fps' frames per second.
-# Use fps=0 for unlimited fps.
-game = DinoGame(60)
 
-# Go to the next frame and take the action 'action'
-# (ACTION_UP, ACTION_FORWARD or ACTION_DOWN).
+#Criação da rede neural
+class NeuralNetwork:
+    def __init__(self):
+        #Define os pesos iniciais de cada entrada como valores aleatorios 0 ou 1
+        self.weights = np.random.rand(11, 3) - 0.5  #-0.5 para que valores sejam negativos ou positivos [-0.5, 0.5]
 
-while(not game.game_over):
-    game.step(ACTION_UP)
+    #Função de predição
+    def predict(self, state):
+        #Faz a multiplicação das entradas do state pela matriz pesos, gerando 3 valores
+        output = np.dot(state, self.weights)
+        #Seleciona qual das 3 saídas do resultado da multiplicação anterior que tem o maior valor (escolha que a rede avaliou como certa)
+        return np.argmax(output)
 
-    # Get a list of floats representing the game state
-    # (positions of the obstacles and game speed).
-    game.get_state()
 
-    # Get the game score.
-    game.get_score()
+def mutate(weights):
+    #Coeficiente de mutação. Indica o quanto o neurônio será mutado
+    mutation_rate = 0.05
+    #Gera uma matriz aleatória multiplicada pelo mutation_rate e mistura com os pesos atuais 
+    return weights + mutation_rate * np.random.randn(*weights.shape)
 
-    # Reset the game.
-game.reset()
+#Criação do objeto do jogo com 100 dinossauros e 60 fps
+game = MultiDinoGame(dino_count=100, fps=60)
 
-    # Close the game.
-game.close()
+#Gera a população com uma rede neural para cada dinossauro
+population = [NeuralNetwork() for _ in range(100)]
+
+#Loop principal do jogo
+while True:
+    actions = []
+    states = game.get_state()
+
+    # Decidir ação de cada dinossauro
+    for i in range(game.dino_count):
+        #Para cada dinossauro, realizar o predict da ação
+        action = population[i].predict(states[i])
+        #Inserir o predict da ação na lista de execução
+        actions.append([ACTION_FORWARD, ACTION_UP, ACTION_DOWN][action])
+
+    # Realizar uma ação baseado na lista de ações a serem executadas
+    game.step(actions)
+
+    #Ao final de cada rodada
+    if game.game_over:
+        # Selecionar qual foi o melhor dinossauro
+        best_index = np.argmax(game.get_scores())
+        best_nn = population[best_index]
+
+        # Criar nova população
+        population = [NeuralNetwork() for _ in range(100)]
+        for i in range(100):
+            #Nova população vai ter os pesos do melhor dinossauro, sofrendo mutações
+            population[i].weights = mutate(best_nn.weights)
+
+        # Proxima rodada
+        game.reset()
